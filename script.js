991ba65d62a3c250b0af207547bbbcb2afe50f77
Lisa Frank <lfrank@c3r12p7.42quebec.com>
2023-07-12 18:09:20 -0400

    task box, buttons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var ul = document.getElementById("taskList");
+var tasks = document.querySelectorAll("#taskList li");
 
 function inputLength() {
 	return input.value.length;
@@ -8,7 +9,32 @@ function inputLength() {
 
 function createListElement() {
 	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
+	var taskText = document.createElement("span");
+	taskText.classList.add("task");
+	taskText.appendChild(document.createTextNode(input.value));
+	li.appendChild(taskText);
+
+	var actions = document.createElement("span");
+	actions.classList.add("actions");
+
+	var checkBtn = document.createElement("button");
+	checkBtn.innerHTML = "Check";
+	checkBtn.innerHTML = "&#x2713;";
+	checkBtn.addEventListener("click", function() {
+		li.classList.toggle("completed");
+	});
+
+	var deleteBtn = document.createElement("button");
+	deleteBtn.innerHTML = "Delete";
+	deleteBtn.innerHTML = "&#x2212;";
+	deleteBtn.addEventListener("click", function() {
+		li.remove();
+	});
+
+	actions.appendChild(checkBtn);
+	actions.appendChild(deleteBtn);
+	li.appendChild(actions);
+
 	ul.appendChild(li);
 	input.value = "";
 }
@@ -26,5 +52,31 @@ function addListAfterKeypress(event) {
 }
 
 button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
 
+// Add buttons to existing tasks
+tasks.forEach(function(task) {
+	var actions = document.createElement("span");
+	actions.classList.add("actions");
+
+	var checkBtn = document.createElement("button");
+	checkBtn.classList.add("check");
+	checkBtn.innerHTML = "&#x2713;";
+	checkBtn.addEventListener("click", function() {
+		task.classList.toggle("completed");
+	});
+
+	var deleteBtn = document.createElement("button");
+	deleteBtn.classList.add("delete");
+	deleteBtn.innerHTML = "&#x2212;";
+	deleteBtn.addEventListener("click", function() {
+		task.remove();
+	});
+
+	actions.appendChild(checkBtn);
+	actions.appendChild(deleteBtn);
+	task.appendChild(actions);
+});
+
+button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
